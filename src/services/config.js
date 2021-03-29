01069609c907ec81dafe3d7a35bfc166368a01bb
JPsonicACE <jp.filiatrault@gmail.com>
2021-03-28 23:35:11 -0400

    done

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,12 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBv44JPPQoImY8w0DDBFSlvEraIE58oQgY",
+  authDomain: "ex3-produits-7e626.firebaseapp.com",
+  projectId: "ex3-produits-7e626",
+  storageBucket: "ex3-produits-7e626.appspot.com",
+  messagingSenderId: "343154393616",
+  appId: "1:343154393616:web:fb4cc05002f29c6dd18095"
 };
 export default firebaseConfig;
 
